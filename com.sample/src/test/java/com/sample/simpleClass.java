9eba19cbccafb5ae6e0cf64fdbccbee5a72cca66
Siva <Siva@192.168.0.7>
2019-12-07 22:12:38 +0530

    Added project to git

---
+++ com.sample/src/test/java/com/sample/simpleClass.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+import org.testng.annotations.Test;
+
+public class simpleClass {
+	
+	@Test
+	public void firstMethod()
+	{
+		System.out.println("First program pushing to git");
+	}
+
+}
